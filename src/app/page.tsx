b02dae31dd8c08f23a98b4ccd80bcbb353ea9fc6
hyu164 <tinghyu0304@gmail.com>
2025-04-22 00:52:46 +0800

    feat: 新增留言功能

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import './globals.css'
 
 export default function Home() {
@@ -8,8 +7,8 @@ export default function Home() {
     className="min-h-screen w-full bg-cover bg-center bg-no-repeat"
     style={{ backgroundImage: "url('/images/backgroud.JPG')" }}
     >
-      <header className="text-center mb-10">
-        <h1 className="text-3xl sm:text-4xl font-serif text-pink-100 leading-relaxed drop-shadow-md">
+   <header className="text-center mb-10 pt-10">
+  <h1 className="text-3xl sm:text-4xl font-serif text-pink-100 leading-relaxed drop-shadow-md">
          這是我寫給周不要再花錢賭博
         </h1>
         <p className="text-lg sm:text-xl font-serif text-pink-50 drop-shadow-md">
@@ -18,27 +17,11 @@ export default function Home() {
       </header>
 
       <main className="flex flex-col items-center space-y-6 w-full max-w-xs">
-        <Link href="/thoughts">
-          <button className="hover:opacity-80 transition">
-            <Image
-              src="/images/button.png"
-              alt="Button Icon"
-              width={300}
-              height={100}
-              className="object-cover rounded-xl"
-            />
-          </button>
-        </Link>
 
-        <Link href="/write">
-          <button className="font-serif bg-pink-300 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-pink-400 transition w-full">
-            💌 前往寫信頁面
-          </button>
-        </Link>
-
-        <Link href="/stars">
-          <button className="font-serif bg-blue-300 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-blue-400 transition w-full">
-            🌠 前往我的宇宙
+        <Link href="/card">
+        <button className="font-serif bg-pink-300 text-white px-6 py-3 
+        rounded-lg shadow-lg hover:bg-pink-400 transition w-full">
+            賭博啦～
           </button>
         </Link>
       </main>
