1d762610ff65377f504882294e6eaafcdd4b622b
hyu164 <tinghyu0304@gmail.com>
2025-04-23 02:32:44 +0800

    “mini12-navbar3”

--- src/app/card2/page.tsx
+++ src/app/card2/page.tsx
@@ -4,7 +4,16 @@ import Link from 'next/link'
 export default function Card2Home() {
   return (
     <div>
-      <Navbar />  {/* 只有這裡有 Navbar */}
+     <nav className="fixed top-0 left-0 right-0 bg-black/80 z-[999]">
+  <div className="max-w-6xl mx-auto px-8 py-3 flex gap-4">
+    <Link href="/card2/1" className="text-white hover:text-pink-400 transition">
+      卡池1
+    </Link>
+    <Link href="/card2/2" className="text-white hover:text-pink-400 transition">
+      卡池2
+    </Link>
+    </div>
+  </nav> {/* 只有這裡有 Navbar */}
       <div
       className="min-h-screen text-white p-4 md:p-20 relative bg-cover bg-center"
       style={{ backgroundImage: "url('/images/background-12.jpg')" }}
