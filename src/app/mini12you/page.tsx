ec50834f31f8a779577ca5c38063ad07c61f3700
hyu164 <tinghyu0304@gmail.com>
2025-04-25 05:36:05 +0800

    “加音樂”

--- src/app/mini12you/page.tsx
+++ src/app/mini12you/page.tsx
@@ -56,7 +56,7 @@ export default function CardRootPage(){
       className="min-h-screen text-white p-4 md:p-20 relative bg-cover bg-center"
       style={{ backgroundImage: "url('/album/mini12 you.JPG')" }}
     >
-      <div className="absolute top-4 left-4 z-50">
+      <div className="absolute top-4 left-4 z-100">
         <Link href="/">
           <button className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-lg shadow transition text-sm md:text-base">
             ← Back
